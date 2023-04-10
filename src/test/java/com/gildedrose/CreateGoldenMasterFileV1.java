d31fa36b670a7f922a9fc0244423a6c6682de544
Bastien Moenaert <bastien.moenaert@persgroep.net>
2023-04-10 17:46:46 +0200

    inverted all if condtions

--- src/test/java/com/gildedrose/CreateGoldenMasterFileV1.java
+++ src/test/java/com/gildedrose/CreateGoldenMasterFileV1.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CreateGoldenMasterFileV1 {
     public static String FILENAME = "src/test/resources/goldenmasterV1.txt";
@@ -14,18 +16,15 @@ public class CreateGoldenMasterFileV1 {
 
         printWriter.println("OMGHAI!");
 
-        Item[] items = new Item[] {
-            new Item("+5 Dexterity Vest", 10, 20), //
-            new Item("Aged Brie", 2, 0), //
-            new Item("Elixir of the Mongoose", 5, 7), //
-            new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-            new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-            new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-            new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-            new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-            // this conjured item does not work properly yet
-//            new Item("Conjured Mana Cake", 3, 6)
-        };
+        List<Item> items = new ArrayList<>();
+        items.add(new Item("+5 Dexterity Vest", 10, 20));
+        items.add(new Item("Aged Brie", 2, 0));
+        items.add(new Item("Elixir of the Mongoose", 5, 7));
+        items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
+        items.add(new Item("Sulfuras, Hand of Ragnaros", -1, 80));
+        items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+        items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49));
+        items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49));
 
         GildedRoseRefactored app = new GildedRoseRefactored(items);
 
