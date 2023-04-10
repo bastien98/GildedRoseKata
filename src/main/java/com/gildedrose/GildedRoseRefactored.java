e2c82b3af6beb8ca4315cfe68e4ccb1e838b2082
Bastien Moenaert <bastien.moenaert@persgroep.net>
2023-04-10 19:36:40 +0200

    moved item.sellin calculation

--- src/main/java/com/gildedrose/GildedRoseRefactored.java
+++ src/main/java/com/gildedrose/GildedRoseRefactored.java
@@ -12,11 +12,12 @@ public class GildedRoseRefactored {
     public void updateQuality() {
         for (Item item : items) {
 
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.sellIn = item.sellIn - 1;
-            }
+//            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+//                item.sellIn = item.sellIn - 1;
+//            }
 
             if (item.name.equals("Aged Brie")) {
+                item.sellIn = item.sellIn - 1;
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
                 }
@@ -26,9 +27,13 @@ public class GildedRoseRefactored {
 
             }
 
-            if(item.quality >0 && !item.name.equals("Sulfuras, Hand of Ragnaros") && !item.name.equals("Backstage passes to a TAFKAL80ETC concert") && !item.name.equals("Aged Brie") ) {
-                item.quality = item.quality - 1;
-                if (item.sellIn < 0) {
+            if(!item.name.equals("Sulfuras, Hand of Ragnaros") && !item.name.equals("Backstage passes to a TAFKAL80ETC concert") && !item.name.equals("Aged Brie") ) {
+                item.sellIn = item.sellIn - 1;
+                if(item.quality >0) {
+                    item.quality = item.quality - 1;
+                }
+
+                if (item.sellIn < 0 && item.quality > 0) {
                     item.quality = item.quality - 1;
                 }
             }
@@ -40,6 +45,7 @@ public class GildedRoseRefactored {
     }
 
     private void updateBackstage(final Item item) {
+        item.sellIn = item.sellIn - 1;
         if (item.quality < 50) {
             item.quality = item.quality + 1;
         }
