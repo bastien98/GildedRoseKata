577fccc2d89d2cf6c0558c669779957a586f5080
Bastien Moenaert <bastien.moenaert@persgroep.net>
2023-04-10 19:18:14 +0200

    isolated all logic for the calculation of backstage passes

--- src/main/java/com/gildedrose/GildedRoseRefactored.java
+++ src/main/java/com/gildedrose/GildedRoseRefactored.java
@@ -37,16 +37,16 @@ public class GildedRoseRefactored {
 
 
             if (item.sellIn < 0) {
-                if (!item.name.equals("Aged Brie")) {
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        item.quality = 0;
-                    } else {
-                        if (item.quality > 0) {
-                            if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                continue;
-                            }
-                            item.quality = item.quality - 1;
+                if (item.name.equals("Aged Brie")) {
+                    continue;
+                }
+                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                } else {
+                    if (item.quality > 0) {
+                        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                            continue;
                         }
+                        item.quality = item.quality - 1;
                     }
                 }
             }
@@ -68,6 +68,10 @@ public class GildedRoseRefactored {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
             }
+        }
+
+        if (item.sellIn < 0) {
+            item.quality = 0;
         }
     }
 
