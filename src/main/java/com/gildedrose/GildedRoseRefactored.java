b64a73668b77913af767153de2e142f945d03791
Bastien Moenaert <bastien.moenaert@persgroep.net>
2023-04-10 19:15:29 +0200

    moved sellin -1 to top of method

--- src/main/java/com/gildedrose/GildedRoseRefactored.java
+++ src/main/java/com/gildedrose/GildedRoseRefactored.java
@@ -11,10 +11,19 @@ public class GildedRoseRefactored {
 
     public void updateQuality() {
         for (Item item : items) {
+
+            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                item.sellIn = item.sellIn - 1;
+            }
+
             if (item.name.equals("Aged Brie")) {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
                 }
+                if (item.quality < 50 && item.sellIn < 0) {
+                    item.quality = item.quality + 1;
+                }
+
             }
 
             if(item.quality >0 && !item.name.equals("Sulfuras, Hand of Ragnaros") && !item.name.equals("Backstage passes to a TAFKAL80ETC concert") && !item.name.equals("Aged Brie") ) {
@@ -25,16 +34,10 @@ public class GildedRoseRefactored {
                 updateBackstage(item);
             }
 
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.sellIn = item.sellIn - 1;
-            }
+
 
             if (item.sellIn < 0) {
-                if (item.name.equals("Aged Brie")) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                } else {
+                if (!item.name.equals("Aged Brie")) {
                     if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         item.quality = 0;
                     } else {
@@ -55,13 +58,13 @@ public class GildedRoseRefactored {
             item.quality = item.quality + 1;
         }
 
-        if (item.sellIn < 11) {
+        if (item.sellIn +1 < 11) {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
             }
         }
 
-        if (item.sellIn < 6) {
+        if (item.sellIn +1 < 6) {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
             }
