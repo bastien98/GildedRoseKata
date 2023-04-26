98da17d8e25bcb177a85afb9bd00e6cfde247297
Bastien Moenaert <bastien.moenaert@persgroep.net>
2023-04-26 10:44:09 +0200

    enhance for loop

--- src/main/java/com/gildedrose/services/GildedRoseRefactored.java
+++ src/main/java/com/gildedrose/services/GildedRoseRefactored.java
@@ -18,8 +18,8 @@ public class GildedRoseRefactored {
     }
 
     public void updateItems() {
-        for (int i = 0; i < items.size(); i++) {
-            items.get(i).update();
+        for (UpdatableItem item : items) {
+            item.update();
         }
     }
 
